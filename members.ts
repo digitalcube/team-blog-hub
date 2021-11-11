2556cba7e33687063b3609b6bf8960da41c8b1cc
Yoshiko Sarakai <yoshiko@digitalcube.jp>
2021-11-11 16:21:38 +0900

    メンバーの情報の更新

--- members.ts
+++ members.ts
@@ -1,111 +1,124 @@
 import { Member } from "@src/types";
 
 export const members: Member[] = [
-  {
-    name: "Kazue Igarashi",
-    role: "",
-    bio:
-      "",
-    avatarSrc: "/avatars/catnose.jpg",
-    sources: [
-      "https://qiita.com/gatespace/feed.atom",
-      "https://gatespace.jp/feed/atom"
-    ],
-    twitterUsername: "gatespace_k",
-    githubUsername: "gatespace",
-    websiteUrl: "https://qiita.com/gatespace.com",
-  },
-  {
-    name: "Wokamoto",
-    role: "",
+	{
+    name: "OHTA Tatsuki",
+    role: "Frontend Engineer",
     bio: "",
-    avatarSrc: "https://gravatar.com/avatar/de6bf7edc1c1d23f52ea9fe22870d6a6?s=400&d=robohash&r=x",
-    sources: ["https://qiita.com/wokamoto/feed.atom"],
-    twitterUsername: "wokamoto",
-    githubUsername: "wokamoto"
-  },
-  {
-    name: "JOTAKI Taisuke",
-    role: "",
-    bio: "",
-    avatarSrc: "/avatars/amanda.jpg",
-    sources: ["https://wp.tekapo.com/feed/atom"],
-    twitterUsername: "wokamoto",
-    githubUsername: "wokamoto"
-  },
-  {
-    name: "plasticdreams",
-    role: "",
-    bio: "",
-    avatarSrc: "/avatars/amanda.jpg",
-    sources: ["https://blog.plasticdreams.org/feed"],
-    twitterUsername: "",
-    githubUsername: ""
-  },
-  {
-    name: "mt8",
-    role: "",
-    bio: "",
-    avatarSrc: "/avatars/amanda.jpg",
-    sources: ["https://mt8.biz/feed"],
-    twitterUsername: "",
-    githubUsername: ""
-  },
-  {
-    name: "Webnist",
-    role: "",
-    bio: "",
-    avatarSrc: "/avatars/junji.jpg",
-    sources: [
-      "https://qiita.com/Webnist/feed.atom"
-    ],
-  },
-  {
-    name: "gorooe",
-    role: "",
-    bio: "",
-    avatarSrc: "/avatars/naoko.jpg",
-    sources: [
-      "https://qiita.com/gorooe/feed.atom"
-    ],
-  },
-  {
-    name: "03hiro",
-    role: "",
-    bio: "",
-    avatarSrc: "/avatars/alexandria.jpg",
-    sources: [
-      "https://qiita.com/03hiro/feed.atom"
-    ],
-  },
-  {
-    name: "Shifter Official",
-    role: "",
-    bio: "",
-    avatarSrc: "https://gravatar.com/avatar/9a5a3f1e26dff03a5ce557371663d0a6?s=400&d=robohash&r=x",
-    sources: [
-      "https://getshifter.io/feed/atom"
-    ],
-  },
-  {
-    name: "AMIMOTO Official",
-    role: "",
-    bio: "",
-    avatarSrc: "https://gravatar.com/avatar/88fce0137a0e5b8bd0dd1cfcc813374a?s=400&d=robohash&r=x",
-    sources: [
-      "https://amimoto-ami.com/feed/atom",
-      "https://ja.amimoto-ami.com/feed/atom"
-    ],
-  },
-  {
-    name: "Digitalcube Official",
-    role: "",
-    bio: "",
-    avatarSrc: "/avatars/digitalcube.jpg",
-    sources: [
-      "https://labworks.digitalcube.jp/feed/atom",
-      "https://digitalcube.jp/feed/atom",
-      "https://en.digitalcube.jp/feed/atom"
-    ],
-  },
+    avatarSrc: "/avatars/ohta.png",
+    sources: ["https://qiita.com/Webnist/feed.atom"],
+	},
+	{
+		name: "JOTAKI Taisuke",
+		role: "Technical Support",
+		bio: "",
+		avatarSrc: "/avatars/jyotaki.jpg",
+		sources: ["https://wp.tekapo.com/feed/atom"],
+		twitterUsername: "",
+		githubUsername: "tekapo"
+	},
+
+{
+	name: "OKAMOTO Wataru",
+	role: "CTO",
+	bio: "",
+	avatarSrc: "https://gravatar.com/avatar/de6bf7edc1c1d23f52ea9fe22870d6a6?s=400&d=robohash&r=x",
+	sources: ["https://qiita.com/wokamoto/feed.atom"],
+	twitterUsername: "wokamoto",
+	githubUsername: "wokamoto"
+},
+{
+	name: "IGARASHI Kazue",
+	role: "Director",
+	bio:
+		"",
+	avatarSrc: "/avatars/igarashi.jpg",
+	sources: [
+		"https://qiita.com/gatespace/feed.atom",
+		"https://gatespace.jp/feed/atom"
+	],
+	twitterUsername: "gatespace_k",
+	githubUsername: "gatespace",
+	websiteUrl: "https://qiita.com/gatespace.com",
+},
+{
+	name: "TSUNODA Tomohyco",
+	role: "Technical Support",
+	bio: "",
+	avatarSrc: "/avatars/tsunoda.jpg",
+	sources: ["https://blog.plasticdreams.org/feed"],
+	twitterUsername: "",
+	githubUsername: "plastikdreams"
+	},
+	{
+		name: "TAKESHITA Kazuto",
+		role: "Engineer",
+		bio: "",
+		avatarSrc: "/avatars/mt8.png",
+		sources: ["https://mt8.biz/feed"],
+		twitterUsername: "",
+		githubUsername: "mt8"
+	},
+	{
+		name: "ONDA Junko",
+		role: "Director of Client Services",
+		bio: "",
+		avatarSrc: "/avatars/onda.jpg",
+		sources: ["https://mamelogy.com/feed"],
+		twitterUsername: "onyun",
+	},
+	{
+		name: "SARAKAI Yoshiko",
+		role: "Frontend Engineer",
+		bio: "",
+		avatarSrc: "/avatars/sarakai.png",
+		sources: [
+			"https://beginners.shifter.community/feed"],
+		twitterUsername: "yotigory",
+		githubUsername: "yotigory"
+	},
+	{
+		name: "OE Goro",
+		role: "Engineer",
+		bio: "",
+		avatarSrc: "/avatars/oe.jpg",
+		sources: ["https://qiita.com/gorooe/feed.atom"],
+	},
+	{
+		name: "OYA Hiroki",
+		role: "Frontend Engineer",
+		bio: "",
+		avatarSrc: "/avatars/oya.jpg",
+		sources: ["https://qiita.com/03hiro/feed.atom"],
+	},
+	{
+		name: "Shifter Official",
+		role: "",
+		bio: "",
+		avatarSrc: "https://gravatar.com/avatar/9a5a3f1e26dff03a5ce557371663d0a6?s=400&d=robohash&r=x",
+		sources: [
+			"https://getshifter.io/feed/atom"
+		],
+	},
+	{
+		name: "AMIMOTO Official",
+		role: "",
+		bio: "",
+		avatarSrc: "https://gravatar.com/avatar/88fce0137a0e5b8bd0dd1cfcc813374a?s=400&d=robohash&r=x",
+		sources: [
+			"https://amimoto-ami.com/feed/atom",
+			"https://ja.amimoto-ami.com/feed/atom"
+		],
+	},
+	{
+		name: "Digitalcube Official",
+		role: "",
+		bio: "",
+		avatarSrc: "/avatars/digitalcube.jpg",
+		sources: [
+			"https://labworks.digitalcube.jp/feed",
+			"https://www.digitalcube.jp/feed",
+			"https://en.digitalcube.jp/feed/atom"
+		],
+	}
 ];
